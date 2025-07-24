31b4606eff5d081616bb38b8fbce224666c5e36b
Sami <samibghanem@gmail.com>
2025-07-24 23:37:44 +0000

    parsing

--- latent_action_models/data_exploration/parse_row.py
+++ latent_action_models/data_exploration/parse_row.py
@@ -1,21 +1,25 @@
-from optparse import Option
-import  pathlib, csv, re, sys
+import  traceback
+from    tqdm            import tqdm
 from    pathlib         import Path
-from    multimethod     import overload
+from    multimethod     import multimethod, parametric
 from    typing          import Iterable, Literal, Optional, Generator, TypedDict, Callable
 from    dataclasses     import dataclass, field, asdict
 from    toolz           import excepts as _excepts, pipe, map, last, first
-from    toolz.curried   import map, concatv
-from    functools       import wraps
+from    toolz.curried   import map, concatv, mapcat
+from    functools       import wraps, singledispatch
 
 
-DATA_ROOT = Path("/mnt/data/shahbuland/video-proc-2/datasets/gta_nas")
+DATA_ROOT       = Path("/mnt/data/shahbuland/video-proc-2/datasets/gta_nas")
+FILE_PROGRESS_BAR    = tqdm(desc='Processing files...')
+LINE_PROGRESS_BAR    = tqdm(desc='Processing lines...')
 
 
 def excepts(exc: type(Exception), handler: Callable):
     def decorator(fn):
         @wraps(fn)
-        def wrapper(*args, **kwargs): return _excepts(exc, fn, handler)
+        def wrapper(*args, **kwargs):
+            return _excepts(exc, fn,
+                            lambda _: handler(*args, **kwargs))(*args, **kwargs)
         return wrapper
     return decorator
 
@@ -23,55 +27,71 @@ def excepts(exc: type(Exception), handler: Callable):
 def iter_csv(root: Path = DATA_ROOT) -> Generator[Path, None, None]:
     yield from root.glob('*.csv')
 
-
-class RowContext(TypedDict):
+@dataclass()
+class RowContext:
     origin_csv:     Path
     origin_vid:     Path
     lineno:         int
     raw_row:        str
 
 
-@dataclass
+@dataclass()
 class ParsedEvent:
-    origin_csv:     Path
-    origin_vid:     Path
-    lineno:         int
-    raw:            str                 = field(repr=False)
-    exc:            Optional[str]       = None
-    t_sec:          Optional[float]
-    event_type:     Optional[Literal["mouse", "key"]]
-    keys_active:    Optional[list[str]]
-    key_pressed:    Optional[str]
-    transition:     Optional[str]
-    dx:             Optional[int]
-    dy:             Optional[int]
-    wheel:          Optional[float]
+    origin_csv:         Path
+    origin_vid:         Path
+    lineno:             int
+    raw:                str                                 = field(repr=False)
+    t_sec:              Optional[float]                     = None
+    event_type:         Optional[Literal["mouse", "key"]]   = None
+    keys_active:        Optional[list[str]]                 = None
+    key_pressed:        Optional[str]                       = None
+    key_pressed_dir:    Optional[Literal['up', 'down']]     = None
+    transition:         Optional[str]                       = None
+    dx:                 Optional[int]                       = None
+    dy:                 Optional[int]                       = None
+    wheel:              Optional[float]                     = None
+    exc:                Optional[str]                       = None
 
-def _first_member   (s: Iterable[str], seek: str) -> Optional[str]:
-    return first(c for c in s if seek in c)
-
-def _from_ctxt      (ctxt: RowContext) -> ParsedEvent:
-    return ParsedEvent( origin_csv = ctxt['origin_csv'],
-                        origin_vid = ctxt['origin_vid'],
-                        lineno     = ctxt['lineno'],
-                        raw        = ctxt['raw'])
-
+def is_keypress     (raw_row: str)  -> bool:
+    return any(('down'   in last(raw_row.split(',')),
+                'up'     in last(raw_row.split(','))))
 
 def is_mouse_delta  (raw_row: str)  -> bool:
-    return last(raw_row.split(',')) == 'mouse event'
+    return 'mouse event' in last(raw_row.split(','))
 
 
-@excepts            (Exception, _from_ctxt)
-@overload
-def _parse_row      (raw_row: is_mouse_delta, ctxt: RowContext) -> ParsedEvent:
+MouseDeltaRow   = parametric(str, is_mouse_delta)
+KeypressRow     = parametric(str, is_keypress)
+
+
+@multimethod
+def _parse_row      (raw_row: str,      ctxt: RowContext) -> ParsedEvent:
+    raise Exception(f'Cannot detect event: \"{raw_row}\" - {ctxt}')
+
+
+def _from_ctxt(raw_row: str, ctxt: RowContext) -> ParsedEvent:
+    return ParsedEvent( origin_csv  = ctxt.origin_csv,
+                        origin_vid  = ctxt.origin_vid,
+                        lineno      = ctxt.lineno,
+                        raw         = ctxt.raw_row,
+                        exc         = exc   if (exc := traceback.format_exc()) != 'NoneType: None\n'
+                                            else None)
+
+
+def _first_member(s: Iterable[str], seek: str) -> Optional[str]:
+    return first(c for c in s if seek in c)
+
+
+@excepts(Exception, _from_ctxt)
+@multimethod
+def _parse_row(raw_row: MouseDeltaRow, ctxt: RowContext) -> ParsedEvent:
     timestamp, data, _  = raw_row.split(',')
     dx, dy, wheel       = tuple(_first_member(data.split(';'), key)
                                 for key in ('pos_x', 'pos_y', 'wheel'))
-    
+
     if None in (dx, dy, wheel):         print(f'[!!] ({dx=} {dy=} {wheel=})')
 
-    sign_x, sign_y      = 1, 1
-
+    sign_x, sign_y        = 1, 1
     if '-' in dx: sign_x *= -1
     if '-' in dy: sign_y *= -1
     
@@ -91,40 +111,55 @@ def _parse_row      (raw_row: is_mouse_delta, ctxt: RowContext) -> ParsedEvent:
 
     return event
 
-def is_keypress     (raw_row: str)  -> bool:
-    return any('down'   in last(raw_row.split(',')),
-               'up'     in last(raw_row.split(',')))
 
-@excepts            (Exception, _from_ctxt)
-@overload
-def _parse_row      (raw_row: is_keypress,    ctxt: RowContext) -> ParsedEvent:
-    timestamp, keys_active, key_pressed = raw_row.split(',')
-    
+@excepts(Exception, _from_ctxt)
+@multimethod
+def _parse_row(raw_row: KeypressRow, ctxt: RowContext) -> ParsedEvent:
+    (   timestamp,
+        keys_raw,
+        keypresses ) = raw_row.split(',')
+
+    if 'none' in keys_raw.lower():  keys_active = []
+    else:                           keys_active = keys_raw.split('+')
+
+    key_pressed, key_pressed_dir    = keypresses.lower().split(' ')
+
+    event                           = _from_ctxt(ctxt)
+    event.event_type                = 'key'
+    event.t_sec                     = float(timestamp)
+    event.keys_active               = keys_active
+    event.key_pressed               = key_pressed
+    event.key_pressed_dir           = key_pressed_dir
+    return event
+
 
 def _preprocess_raw (raw_row: str)  -> str: return raw_row.strip().lower()
 
 def process_file(path: Path)        -> Generator[ParsedEvent, None, None]:
     with open(path, 'r') as f:
+        FILE_PROGRESS_BAR.set_description(f'Processing files... {path} - lines: {len(f.readlines())}') ; f.seek(0)
         yield from (
             _parse_row(
                 _preprocess_raw(line),
-                ctxt=RowContext(
-                origin_csv  = path,
-                origin_vid  = path.with_suffix('.mkv'),
-                lineno      = i,
-                raw_row     = line
-        )) 
+                RowContext(
+                    origin_csv  = path,
+                    origin_vid  = path.with_suffix('.mkv'),
+                    lineno      = i,
+                    raw_row     = line
+                )
+            ) 
             for i,  line in enumerate(f.readlines())
-            if      line != ''
+            if     (line != '') ^ bool(LINE_PROGRESS_BAR.update(1))
         )
+
+        FILE_PROGRESS_BAR.update(1)
 
 
 def process_dir (root: Path = DATA_ROOT)   -> Generator[ParsedEvent, None, None]:
     yield from pipe(
         root,
         iter_csv,
-        map(process_file),
-        concatv
+        mapcat(process_file)
     )
 
 if __name__ == "__main__":
