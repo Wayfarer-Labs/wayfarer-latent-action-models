b449831c517b4b899573ac43690674fbbf818b66
Sami <samibghanem@gmail.com>
2025-08-04 20:46:05 +0000

    vae bridge

---
+++ latent_action_models/vae_bridge/owl_vae_bridge.py
@@ -0,0 +1,35 @@
+import os, sys, yaml, torch
+import omegaconf
+
+sys.path.append("./owl_vaes")
+
+from owl_vaes import from_pretrained
+
+
+def load_cfg(path):
+    with open(path, 'r') as f:
+        cfg = omegaconf.OmegaConf.create(yaml.safe_load(f))
+        if hasattr(cfg, 'model'): return cfg.model
+        return cfg
+
+
+class OWLDecodingPipeline:
+    def __init__(self, cfg_path, ckpt_path):
+        model = from_pretrained(cfg_path, ckpt_path)
+        self.model = model.decoder.eval().bfloat16().cuda()
+        del model.encoder
+        self.model = torch.compile(self.model, dynamic=False, fullgraph=True)
+
+    @torch.no_grad()
+    def __call__(self, x):
+        return self.model(x)
+
+def R3DCDecodingPipeline():
+    return OWLDecodingPipeline(
+        "/mnt/data/shahbuland/owl-vaes/configs/1x/no_depth.yml",
+        "/mnt/data/shahbuland/owl-vaes/checkpoints/1x_rgb_no_depth/step_200000.pt"
+    )
+
+if __name__ == "__main__":
+    x = R3DCDecodingPipeline()
+    pass
